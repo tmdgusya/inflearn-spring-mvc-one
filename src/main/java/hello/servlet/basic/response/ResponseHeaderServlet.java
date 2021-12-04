42c9ec483604627d8eca7d4889a438f6decd5203
jsh <tmdgusya@naver.com>
2021-12-04 22:51:26 +0900

    ResponseHeader

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,11 +15,25 @@ public class ResponseHeaderServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(HttpServletResponse.SC_OK);
 
-        resp.setHeader("content-type", "text/plain");
-        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        content(resp);
+
         resp.setHeader("Pragma", "no-cache");
         resp.setHeader("my-header", "hello");
 
+        cookie(resp);
+
         resp.getWriter().write("OK");
     }
+
+    private void content(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setContentType("text/plain");
+        httpServletResponse.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse httpServletResponse) {
+        Cookie cookie = new Cookie("myCookie", "isGood");
+        cookie.setMaxAge(600);
+        httpServletResponse.addCookie(cookie);
+    }
+
 }
